4df176cb10196e1a009849e0315d6bf1e00e114b
shettytas <shettytasya@gmail.com>
2026-04-16 23:20:28 +0530

    commit_create takes the staged files and creates a permanent snapshot

--- commit.c
+++ commit.c
@@ -197,5 +197,34 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
     // TODO: Implement commit creation
     // (See Lab Appendix for logical steps)
     (void)message; (void)commit_id_out;
-    return -1;
+    ObjectID tree;
+    if (tree_from_index(&tree) != 0) return -1;
+
+    Commit c;
+    c.tree = tree;
+
+    if (head_read(&c.parent) == 0) {
+        c.has_parent = 1;
+    } else {
+        c.has_parent = 0;
+    }
+
+    snprintf(c.author, sizeof(c.author), "%s", pes_author());
+    c.timestamp = (uint64_t)time(NULL);
+    snprintf(c.message, sizeof(c.message), "%s", message);
+
+    void *data;
+    size_t len;
+    if (commit_serialize(&c, &data, &len) != 0) return -1;
+
+    if (object_write(OBJ_COMMIT, data, len, commit_id_out) != 0) {
+        free(data);
+        return -1;
+    }
+
+    free(data);
+
+    if (head_update(commit_id_out) != 0) return -1;
+
+    return 0;
 }
